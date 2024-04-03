3161a0b310c56bacad78be00866654ad44783677
diogo <ggdiogogg@gmail.com>
2024-04-03 19:20:17 -0300

    feat: atualizando curso react

--- ignite/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ ignite/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from  'phosphor-react'
 
 export function Sidebar() {
     return (
@@ -6,12 +7,14 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=30&w=600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/126338371?v=4" alt="" />
                 <strong>Diogo Prado</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
