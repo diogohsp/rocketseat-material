ed9192c1bb50e13a826ff5f423fa2e31d0b1e2d9
diogo <ggdiogogg@gmail.com>
2024-04-02 22:33:56 -0300

    feat: atualizando curso

--- ignite/aulas/01-fundamentos-reactjs/src/App.jsx
+++ ignite/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,17 +1,25 @@
 import { Post } from "./Post";
 import { Header } from "./components/header";
+import { Sidebar } from "./components/Sidebar";
 
-import "./styles.css";
+import "./global.css";
+import styles from './App.module.css';
 
 export function App() {
   return (
     <div>
       <Header/>
-      <Post author="Diego NSC" content="Demoro mano" />
-      <Post
-        author="Igor"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint ab temporibus distinctio earum veritatis? Recusandae ipsam possimus mollitia illo ut placeat non obcaecati, nam cumque ab harum vero iure tenetur."
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+        <Post author="Diego NSC" content="Demoro mano" />
+        <Post
+          author="Igor"
+          content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint ab temporibus distinctio earum veritatis? Recusandae ipsam possimus mollitia illo ut placeat non obcaecati, nam cumque ab harum vero iure tenetur."
+        />
+        </main>
+      </div>
     </div>
   );
 }
